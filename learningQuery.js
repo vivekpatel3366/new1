b7351046f9a4a5c70e33a4d482d197c1b912f646
vivekpatel <vivekpatel3366@gmail.com>
2025-02-04 10:16:33 +0530

    learning js file add

---
+++ learningQuery.js
@@ -0,0 +1,27 @@
+/*
+  How to load jQuery library in js file?
+   jQuery(document).ready(function(){
+   
+   })
+  Important topic in jQuery.
+   1] selecter = selecter is a one type of identification for tag.
+       here 5 types of selecter available
+       1] normal selecter
+          -id(#idname)
+          -class(.classname)
+          -name(element name)
+       2] combinater selecter.
+          -descendent selecter
+          -child selecter
+          -genarel sibling
+          -adjusent sibling
+       3] psuedoclass selecter
+       4] psuedo element selecter
+       5] attribute selecter
+   2] event
+   3] method 
+*/
+jQuery(document).ready(function() {
+    console.log('jquery library is loaded');
+
+})
